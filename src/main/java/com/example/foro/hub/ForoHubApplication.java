1cb54c27ffc5a065924251dd9b645441fffa95ef
Darckbell <phngilberto@gmail.com>
2024-07-15 23:11:41 -0600

    primer commit

---
+++ src/main/java/com/example/foro/hub/ForoHubApplication.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.example.foro.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForoHubApplication.class, args);
+        System.out.println("works!");
+    }
+}
